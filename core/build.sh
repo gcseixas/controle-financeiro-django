53eed38c09906cd389094de98faba01339fd2ec8
Gabriel Costa <gcseixas01@gmail.com>
2026-02-07 18:25:49 -0300

    Ajustes de configuração

--- core/build.sh
+++ core/build.sh
@@ -2,5 +2,5 @@
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-python core/manage.py collectstatic --noinput
-python core/manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate
