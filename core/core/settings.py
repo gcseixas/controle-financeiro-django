f65f20bd2aabf23fd0e77aa4889553ccc0516fbf
Gabriel Costa <gcseixas01@gmail.com>
2026-02-07 16:22:56 -0300

    Ajusta settings para produção com variáveis de ambiente

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-by_u5^ttc%ir90my6k%z#%p%w9^9lthh109b&0ruk=$q6dgje+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,9 +118,6 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'app.User'
