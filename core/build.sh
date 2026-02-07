64e81802ecba0f6420d81db3a8495d41f31817e6
Gabriel Costa <gcseixas01@gmail.com>
2026-02-07 19:32:31 -0300

    Ajustes de configuração

--- core/build.sh
+++ core/build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-set -o errexit
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-
-python core/manage.py collectstatic --noinput
-python core/manage.py migrate
-python core/manage.py createsuperuser --noinput || true
+python manage.py collectstatic --noinput
+python manage.py migrate
