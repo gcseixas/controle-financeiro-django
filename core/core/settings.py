9f94e5b4af8b968d3d6709a09e1ffe8efd8cf2b1
Gabriel Costa <gcseixas01@gmail.com>
2026-02-07 16:11:13 -0300

    Configuração para deploy em produção (Render)

--- core/core/settings.py
+++ core/core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-by_u5^ttc%ir90my6k%z#%p%w9^9lthh109b&0ruk=$q6dgje+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
