d150627c13af3ace19174608ab7048e518c0d434
Gabriel Costa <gcseixas01@gmail.com>
2026-02-17 08:15:46 -0300

    Ajuste no database

--- core/core/settings.py
+++ core/core/settings.py
@@ -78,13 +78,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# Database
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
